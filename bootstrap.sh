41a905beebdae14557972402afb2d4f87bee99ca
Nick Manganelli <nicholas.james.manganelli@cern.ch>
2024-06-14 08:05:20 +0200

    Update image, add install of dasmonoz package, add voms-proxy-init

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,16 +3,16 @@
 cat <<EOF > shell
 #!/usr/bin/env bash
 
-voms-proxy-init -voms cms --valid 192:00 --out \$HOME/x509up_u\$UID
+# voms-proxy-init -voms cms -vomses /etc/grid-security/vomses/ --valid 192:00 --out \$HOME/x509up_u\$UID
 
 if [[ "\$1" == "" ]]; then
-  export COFFEA_IMAGE="coffeateam/coffea-dask-almalinux8:2024.4.1-py3.11"
+  export COFFEA_IMAGE="coffeateam/coffea-dask-almalinux8:2024.5.0-py3.11"
   # export COFFEA_IMAGE=/cvmfs/unpacked.cern.ch/registry.hub.docker.com/coffeateam/coffea-dask:latest
 else
   export COFFEA_IMAGE=/cvmfs/unpacked.cern.ch/registry.hub.docker.com/\$1
 fi
 
-export APPTAINER_BINDPATH=/cvmfs,/cvmfs/grid.cern.ch/etc/grid-security:/etc/grid-security
+export APPTAINER_BINDPATH=/cvmfs,/cvmfs/grid.cern.ch/etc/grid-security:/etc/grid-security,/eos
 
 APPTAINER_SHELL=\$(which bash) apptainer exec -B \${PWD}:/srv --pwd /srv \\
   /cvmfs/unpacked.cern.ch/registry.hub.docker.com/\${COFFEA_IMAGE} \\
@@ -28,9 +28,12 @@ install_env() {
   unlink .env/lib64  # HTCondor can't transfer symlink to directory and it appears optional
   # work around issues copying CVMFS xattr when copying to tmpdir
   export TMPDIR=\$(mktemp -d -p .)
-  .env/bin/python -m ipykernel install --user
   rm -rf \$TMPDIR && unset TMPDIR
-  .env/bin/python -m pip install -q git+https://github.com/CoffeaTeam/lpcjobqueue.git@v\${LPCJQ_VERSION}
+  # .env/bin/python -m pip install --upgrade awkward dask_awkward coffea uproot
+  cd processing
+  ../.env/bin/python -m pip install -e .
+  cd ..
+  # .env/bin/python -m pip install -q git+https://github.com/CoffeaTeam/lpcjobqueue.git@v\${LPCJQ_VERSION}
   echo "done."
 }
 
@@ -55,6 +58,7 @@ unset GREP_OPTIONS
 [[ -d .env ]] || install_all
 source .env/bin/activate
 alias pip="python -m pip"
+voms-proxy-init -voms cms -vomses /etc/grid-security/vomses/ --valid 192:00 --out \$HOME/x509up_u\$UID
 pip show lpcjobqueue 2>/dev/null | grep -q "Version: \${LPCJQ_VERSION}" || pip install -q git+https://github.com/CoffeaTeam/lpcjobqueue.git@v\${LPCJQ_VERSION}
 EOF
 
